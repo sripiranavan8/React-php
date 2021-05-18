ccc6ec1f2fda017d2850b3fed4c9b727aa80d5cc
Sripiranavan Yogarajah <sripiranavan08@gmail.com>
2021-05-19 04:04:43 +0530

    Socket

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ require __DIR__ . '/vendor/autoload.php';
 
 use React\EventLoop\Factory;
 use React\Promise\Deferred;
+use React\Socket\Connection;
+use React\Socket\LimitingServer;
+use React\Socket\Server;
 use React\Stream\CompositeStream;
 use React\Stream\ReadableResourceStream;
 use React\Stream\ThroughStream;
@@ -45,38 +48,55 @@ use React\Stream\WritableResourceStream;
 // $loop->run();
 
 // function get($uri, $successCallback, $errorCallback)
-function get($uri)
-{
-    $deferred = new Deferred();
-    $responseData = "some data";
+// function get($uri)
+// {
+//     $deferred = new Deferred();
+//     $responseData = "some data";
 
-    // if ($responseData) {
-    //     $successCallback($responseData);
-    //     return;
-    // }
-    // $errorCallback(new Exception('no response data'));
-    if ($responseData) {
-        $deferred->resolve($responseData);
-    } else {
-        $deferred->reject(new Exception('no response data'));
-    }
+//     // if ($responseData) {
+//     //     $successCallback($responseData);
+//     //     return;
+//     // }
+//     // $errorCallback(new Exception('no response data'));
+//     if ($responseData) {
+//         $deferred->resolve($responseData);
+//     } else {
+//         $deferred->reject(new Exception('no response data'));
+//     }
 
-    return $deferred->promise();
-}
+//     return $deferred->promise();
+// }
 
-// get('https://sripiranavan.com', function ($data) {
-//     echo 'Received data: ' . $data;
-// }, function (Exception $e) {
-//     echo "Error: " . $e->getMessage();
-// });
+// // get('https://sripiranavan.com', function ($data) {
+// //     echo 'Received data: ' . $data;
+// // }, function (Exception $e) {
+// //     echo "Error: " . $e->getMessage();
+// // });
 
-get('https://sripiranavan.com')
-    ->then(function ($data) {
-        throw new RuntimeException('Whoops!!!');
-        return strtoupper($data);
-    })
-    ->then(function ($data) {
-        echo "Received data: " . $data;
-    }, function (Exception $e) {
-        echo "Error: " . $e->getMessage();
+// get('https://sripiranavan.com')
+//     ->then(function ($data) {
+//         throw new RuntimeException('Whoops!!!');
+//         return strtoupper($data);
+//     })
+//     ->then(function ($data) {
+//         echo "Received data: " . $data;
+//     }, function (Exception $e) {
+//         echo "Error: " . $e->getMessage();
+//     });
+
+$loop = Factory::create();
+$out =  new WritableResourceStream(STDOUT, $loop);
+$server = new Server('0.0.0.0:8000', $loop);
+$limittingServer = new LimitingServer($server, null);
+
+$limittingServer->on('connection', function (Connection $connection) use ($out, $limittingServer) {
+    // echo "New Connection";
+    // $out->write("New Connection" . PHP_EOL);
+    $connection->on('data', function ($data) use ($out, $limittingServer) {
+        foreach ($limittingServer->getConnections() as $connection) {
+            $connection->write($data);
+        }
+        // $out->write($data);
     });
+});
+$loop->run();
