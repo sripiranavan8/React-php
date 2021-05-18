1ffb0ff0a94227104f93ad5521ec46efd1e047d0
Sripiranavan Yogarajah <sripiranavan08@gmail.com>
2021-05-19 03:46:38 +0530

    promise

--- index.php
+++ index.php
@@ -2,16 +2,17 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use React\EventLoop\Factory;
+use React\Promise\Deferred;
 use React\Stream\CompositeStream;
 use React\Stream\ReadableResourceStream;
 use React\Stream\ThroughStream;
 use React\Stream\WritableResourceStream;
 
-$loop = Factory::create();
+// $loop = Factory::create();
 // $loop->addPeriodicTimer(1, function () {
 //     echo "Hello" . PHP_EOL;
 // });
-$out = new WritableResourceStream(STDOUT, $loop);
+// $out = new WritableResourceStream(STDOUT, $loop);
 // $in = new ReadableResourceStream(STDIN, $loop);
 
 // $in->on('data', function ($data) use ($out) {
@@ -30,15 +31,52 @@ $out = new WritableResourceStream(STDOUT, $loop);
 // });
 
 // Change the size
-$in = new ReadableResourceStream(STDIN, $loop, 1);
-$loop->addPeriodicTimer(0.2, function () {
-    echo 'Hello' . PHP_EOL;
-});
-$in->on('data', function ($data) use ($out, $in, $loop) {
-    $out->write($data . PHP_EOL);
-    $in->pause();
-    $loop->addTimer(1, function () use ($in) {
-        $in->resume();
+// $in = new ReadableResourceStream(STDIN, $loop, 1);
+// $loop->addPeriodicTimer(0.2, function () {
+//     echo 'Hello' . PHP_EOL;
+// });
+// $in->on('data', function ($data) use ($out, $in, $loop) {
+//     $out->write($data . PHP_EOL);
+//     $in->pause();
+//     $loop->addTimer(1, function () use ($in) {
+//         $in->resume();
+//     });
+// });
+// $loop->run();
+
+// function get($uri, $successCallback, $errorCallback)
+function get($uri)
+{
+    $deferred = new Deferred();
+    $responseData = "some data";
+
+    // if ($responseData) {
+    //     $successCallback($responseData);
+    //     return;
+    // }
+    // $errorCallback(new Exception('no response data'));
+    if ($responseData) {
+        $deferred->resolve($responseData);
+    } else {
+        $deferred->reject(new Exception('no response data'));
+    }
+
+    return $deferred->promise();
+}
+
+// get('https://sripiranavan.com', function ($data) {
+//     echo 'Received data: ' . $data;
+// }, function (Exception $e) {
+//     echo "Error: " . $e->getMessage();
+// });
+
+get('https://sripiranavan.com')
+    ->then(function ($data) {
+        throw new RuntimeException('Whoops!!!');
+        return strtoupper($data);
+    })
+    ->then(function ($data) {
+        echo "Received data: " . $data;
+    }, function (Exception $e) {
+        echo "Error: " . $e->getMessage();
     });
-});
-$loop->run();
